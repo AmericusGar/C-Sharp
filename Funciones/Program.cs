2c3c32308badf6e45309d69071835f7a9d0a4764
América Garmendia <85145783+AmericusGar@users.noreply.github.com>
2021-07-13 06:49:12 -0600

    Add files via upload

---
+++ Funciones/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+ namespace funciones
+{
+     class Program
+     {
+         
+        static  void  Main (string [] argumentos )
+        {
+            Alumno a  =  new  alumno ( 1 , " Juan " , " Pérez " );
+            Alumno  b  =  new alumno ( 2 , " Maria " , " Martínez " );
+
+            a.inactivarAlumno ();
+
+            Console. WriteLine ( a . NombreCompleto ());
+            Console. WriteLine ( b . NombreCompleto ());
+
+            a.activarAlumno ();
+
+            Console.WriteLine ( " " );
+
+            Console.WriteLine ( a . NombreCompleto ());
+            Console.WriteLine ( b . NombreCompleto ());
+        }
+    }
+}
